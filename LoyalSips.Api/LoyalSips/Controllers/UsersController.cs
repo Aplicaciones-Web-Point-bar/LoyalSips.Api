5bab739bada6cc81762baf37957132cb67511e15
leonardo <u202124304@upc.edu.pe>
2023-10-18 20:37:06 -0500

    123

--- LoyalSips.Api/LoyalSips/Controllers/UsersController.cs
+++ LoyalSips.Api/LoyalSips/Controllers/UsersController.cs
@@ -9,75 +9,73 @@ namespace LoyalSips.Api.LoyalSips.Controllers;
 
 [ApiController]
 [Route("/api/v1/[controller]")]
-public class BarController : ControllerBase
+public class UsersController: ControllerBase
 {
-    private readonly IBarService _barService;
+    private readonly IUserService _userService;
     private readonly IMapper _mapper;
 
-    public BarController(IBarService barService, IMapper mapper)
+    public UsersController(IUserService userService, IMapper mapper)
     {
-        _barService = barService;
+        _userService = userService;
         _mapper = mapper;
     }
-    
     [HttpGet]
-    public async Task<IEnumerable<BarResource>> GetAllAsync()
+    public async Task<IEnumerable<UserResource>> GetAllAsync()
     {
-        var pubs = await _barService.ListAsync();
-        var resources = _mapper.Map<IEnumerable<Bar>,
-            IEnumerable<BarResource>>(pubs);
+        var users = await _userService.ListAsync();
+        var resources = _mapper.Map<IEnumerable<User>,
+            IEnumerable<UserResource>>(users);
         return resources;
     }
     
     [HttpPost]
-    public async Task<IActionResult> PostAsync([FromBody] SaveBarResource resource)
+    public async Task<IActionResult> PostAsync([FromBody] SaveUserResource resource)
     {
         //verifica si los datos recibidos de resource cumplen las reglas.
         if (!ModelState.IsValid)
             return BadRequest(ModelState.GetErrorMessages());
         
         //mapea el java saveCategoryResource a un objeto CategoryLL
-        var bar = _mapper.Map<SaveBarResource,
-            Bar>(resource);
+        var user = _mapper.Map<SaveUserResource, User>(resource);
         
         //agrega la categoria mediante en categoryService, si falla da el mensaje de estado, o en que se incumplio
-        var result = await _barService.SaveAsync(bar);
+        var result = await _userService.SaveAsync(user);
         if (!result.Success)
             return BadRequest(result.Message);
         
         //mapea el objeto CategoryLL a el json CategoryResource
-        var barResource = _mapper.Map<Bar,
-            BarResource>(result.Resource);
+        var userResource = _mapper.Map<User,
+            UserResource>(result.Resource);
         
         //muestra la respuesta de OK(200) y la categoria creada en JSON
-        return Ok(barResource);
+        return Ok(userResource);
     }
     [HttpPut("{id}")]
-    public async Task<IActionResult> PutAsync(int id, [FromBody] SaveBarResource resource)
+    public async Task<IActionResult> PutAsync(int id, [FromBody] SaveUserResource resource)
     {
         if (!ModelState.IsValid)
             return BadRequest(ModelState.GetErrorMessages());
 
-        var bar = _mapper.Map<SaveBarResource,
-            Bar>(resource);
-        var result = await _barService.UpdateAsync(id, bar);
+        var user = _mapper.Map<SaveUserResource,
+            User>(resource);
+        var result = await _userService.UpdateAsync(id, user);
 
         if (!result.Success)
             return BadRequest(result.Message);
         
-        var barResource = _mapper.Map<Bar,
-            BarResource>(result.Resource);
-        return Ok(barResource);
+        var userResource = _mapper.Map<User,
+            UserResource>(result.Resource);
+        return Ok(userResource);
     }
     [HttpDelete("{id}")]
     public async Task<IActionResult> DeleteAsync(int id)
     {
-        var result = await _barService.DeleteAsync(id);
+        var result = await _userService.DeleteAsync(id);
         if (!result.Success)
             return BadRequest(result.Message);
 
-        var barResource = _mapper.Map<Bar,
-            BarResource>(result.Resource);
-        return Ok(barResource);
+        var userResource = _mapper.Map<User,
+            UserResource>(result.Resource);
+        return Ok(userResource);
     }
 }
